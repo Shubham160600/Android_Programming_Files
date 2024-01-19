4a0ad9bc6ed3f3dee0900f6ecebc6d6c883ad387
Shubham Saurabh <74124097+Shubham160600@users.noreply.github.com>
2024-01-19 14:28:38 +0530

    Delete popup.js

--- popup.js
+++
@@ -1,9 +0,0 @@
-// popup.js
-document.addEventListener('DOMContentLoaded', function () {
-  var playButton = document.getElementById('playButton');
-
-  playButton.addEventListener('click', function () {
-    alert('Ludo game is starting!');
-    // Add your game logic here
-  });
-});
